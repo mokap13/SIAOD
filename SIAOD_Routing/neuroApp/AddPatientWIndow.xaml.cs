950dbc8a1cbb01671e4ad80bc8dd0351a2a05da3
user <user@PC>
2017-10-05 00:13:38 +0400

    добавлен расчет ИМТ

--- SIAOD_Routing/neuroApp/AddPatientWIndow.xaml.cs
+++ SIAOD_Routing/neuroApp/AddPatientWIndow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,29 @@ namespace neuroApp
         public AddPatientWIndow()
         {
             InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox_TextChanged_1(sender,e);
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            if((textBox_patientHeight.Text != String.Empty)
+                &&textBox_patientWeigth.Text != String.Empty)
+            {
+                var weight = double.Parse(textBox_patientWeigth.Text);
+                var heigth = double.Parse(textBox_patientHeight.Text);
+                var IMT = Math.Round( (weight / ((heigth * heigth) / 10_000)),2);
+                label_IMT.Content = string.Format(IMT.ToString(),"000");
+            }
         }
     }
 }
